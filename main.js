b56db7a8d4a14004f2f55cae117431156e1c4682
abdelilahelhaddad <elhaddadabdelilah@gmail.com>
2021-03-04 18:16:36 +0100

    :iphone: :seedling: Mobile Responsive :sparkles: :iphone:

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const burgerMenu = document.querySelector(".burger");
+const list = document.querySelector(".list");
 
 burgerMenu.addEventListener("click", (e)=>{
   e.preventDefault();
   burgerMenu.classList.toggle("active");
+  if (burgerMenu.classList.contains("active")) {
+    list.classList.add("active");
+  }else{
+    list.classList.remove("active");
+  }
 });
